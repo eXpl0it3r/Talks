12fdc3a20fee1b97cddc6dd67f747acc47cefb9b
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2024-03-04 00:01:36 +0100

    Add first demo for Mockoon

---
+++ 2024 - Team Meeting/Mockoon/Demo01/Program.cs
@@ -0,0 +1,8 @@
+﻿var client = new HttpClient();
+client.BaseAddress = new Uri("http://localhost:3001");
+
+var content = new StringContent("abc123");
+var response = await client.PostAsync("login", content);
+response.EnsureSuccessStatusCode();
+var body = await response.Content.ReadAsStringAsync();
+Console.WriteLine($"Response: {body}");
