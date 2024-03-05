51d67887cb683bf5dfd367bc6585061af1417c4e
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2024-03-05 14:27:30 +0100

    Add Demo02 with the Clockify API

---
+++ 2024 - Team Meeting/Mockoon/Demo02/Program.cs
@@ -0,0 +1,20 @@
+﻿using Clockify.Net;
+
+var client = new ClockifyClient("MyApiKey", "http://localhost:3002/api/v1");
+
+var workspaces = await client.GetWorkspacesAsync();
+if (!workspaces.IsSuccessful || workspaces.Data == null)
+{
+    Console.WriteLine($"ERROR: Unable to retrieve Workspaces: {workspaces.ErrorMessage}");
+    return;
+}
+const string clientName = "MyClient";
+var workspaceId = workspaces.Data?.SingleOrDefault(w => w.Name == clientName)?.Id;
+
+if (workspaceId is null)
+{
+    Console.WriteLine($"ERROR: Workspace '{clientName}' could not be found");
+    return;
+}
+
+Console.WriteLine($"Workspace ID: {workspaceId}");
